25eecc4aadbbe60830df139c9e7a37e222e6d937
demenkov_as <demenkov_as>
2020-07-15 13:18:19 +0300

    delete imf from platform

--- resources/views/platforms/index.blade.php
+++ resources/views/platforms/index.blade.php
@@ -4,7 +4,6 @@
     @foreach($platforms as $platform)
         <a href="{{ route('platform.index', ['platform'=>$platform->name]) }}">
             {{ $platform->name }}
-            <img src="{{ $platform->imageSrc }}" alt="{{ $platform->name }}">
         </a>
         <br>
     @endforeach
